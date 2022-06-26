8f1821f02a1ca8e7f9c94c7c04d8514042ff34a7
Joey H <joeyholtzman0507@gmail.com>
2022-06-26 18:04:22 -0500

    Improve method of sending newly scheduled flight notifications
    This feature still has the same functionality, it is just implemented
    better. At the moment, this complexity isn't needed (the previous flight
    length is always 0), but it will make it future-proof when I add a feature
    that checks an account every day for new flights.

--- lib/account.py
+++ lib/account.py
@@ -27,27 +27,24 @@ class Account:
         self.config = Config()
 
     def get_flights(self) -> None:
+        prev_flight_len = len(self.flights)
+
         webdriver = WebDriver()
         reservations = webdriver.get_info(self)
 
-        flight_schedule_message = f"Scheduling the following flights to check in for {self.first_name} {self.last_name}:\n"
         for reservation in reservations:
             confirmation_number = reservation['confirmationNumber']
-            flight_schedule_message += self._get_reservation_info(confirmation_number)
+            self._get_reservation_info(confirmation_number)
 
-        # Only send the message if new flights were scheduled
-        if flight_schedule_message.count('\n') > 1:
-            self.send_notification(flight_schedule_message, NotificationLevel.INFO)
+        self._send_new_flight_notifications(prev_flight_len)
 
     def get_checkin_info(self, confirmation_number: str) -> None:
+        prev_flight_len = len(self.flights)
+
         self.refresh_headers()
+        self._get_reservation_info(confirmation_number)
 
-        flight_schedule_message = f"Scheduling the following flights to check in for {self.first_name} {self.last_name}:\n"
-        flight_schedule_message += self._get_reservation_info(confirmation_number)
-
-        # Only send the message if new flights were scheduled
-        if flight_schedule_message.count('\n') > 1:
-            self.send_notification(flight_schedule_message, NotificationLevel.INFO)
+        self._send_new_flight_notifications(prev_flight_len)
 
     def refresh_headers(self) -> None:
         webdriver = WebDriver()
@@ -70,15 +67,24 @@ class Account:
         # If multiple flights are under the same confirmation number, it will schedule all checkins one by one
         flight_info = response['viewReservationViewPage']['bounds']
 
-        flight_schedule_message = ""
         for flight in flight_info:
             # if not flight in self.flights: // TODO: This doesn't work. Add a function to make sure it only schedules if it isn't already added
             if flight['departureStatus'] != "DEPARTED":
                 flight = Flight(self, confirmation_number, flight)
                 self.flights.append(flight)
-                flight_schedule_message += f"Flight from {flight.departure_airport} to {flight.destination_airport} at {flight.departure_time} UTC\n"
 
-        return flight_schedule_message
+    # Sends new flight notifications to the user. It detects new flights by getting every scheduled flight after
+    # the previous length of the flights list.
+    def _send_new_flight_notifications(self, prev_flight_len: int) -> None:
+        if len(self.flights) == prev_flight_len:
+            # Don't send any notifications if no new flights have been scheduled
+            return
+
+        flight_schedule_message = f"Successfully scheduled the following flights to check in for {self.first_name} {self.last_name}:\n"
+        for flight in self.flights[prev_flight_len:]:
+            flight_schedule_message += f"Flight from {flight.departure_airport} to {flight.destination_airport} at {flight.departure_time} UTC\n"
+
+        self.send_notification(flight_schedule_message, NotificationLevel.INFO)
 
     def send_notification(self, body: str, level: int = None) -> None:
         if len(self.config.notification_urls) == 0:
