f88f02625d9a443356c9449bb00f17294da91313
Everett Southwick <everett@southwick.dev>
2023-02-27 00:37:13 -0500

    Update dockerfile to respect chrome_version

--- Dockerfile
+++ Dockerfile
@@ -5,19 +5,12 @@ WORKDIR /app
 # Needed for python to show logs in all processes
 ENV PYTHONUNBUFFERED 1
 
-RUN apt-get update && apt-get install -y python3 python3-pip wget
-
-# Download google-chrome
-RUN wget https://dl-ssl.google.com/linux/linux_signing_key.pub -O /tmp/google.pub && \
-    gpg --no-default-keyring --keyring /etc/apt/keyrings/google-chrome.gpg --import /tmp/google.pub && \
-    echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main' | \
-    tee /etc/apt/sources.list.d/google-chrome.list
-
-RUN apt-get update && apt-get -y install google-chrome-stable
-
-COPY requirements.txt requirements.txt
-RUN pip3 install --no-cache-dir -r requirements.txt
+RUN apt-get update && apt-get install -y python3 python3-pip wget curl
 
 COPY . .
+RUN pip3 install --no-cache-dir -r requirements.txt
+
+COPY ./utils/chrome-install.py chrome-install.py
+RUN python3 chrome-install.py
 
 ENTRYPOINT ["python3", "southwest.py"]
---
+++ utils/chrome-install.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Chrome installation script for the docker image"""
+
+import os
+import requests
+import json
+
+# Load config
+file = open('config.json')
+config = json.load(file)
+chrome_version = config['chrome_version']
+
+def get_default_version(data):
+    return data['versions'][0]
+
+def get_chrome_version_to_install():
+    response = requests.get('https://versionhistory.googleapis.com/v1/chrome/platforms/linux/channels/stable/versions')
+    if response.status_code == 200:
+        data = response.json()
+
+        # If the config file does not have the chrome_version specified, return default
+        if chrome_version is None:
+            return get_default_version(data)
+        
+        # Iterate through the list of versions until we find the latest minor/patch version for the major version we're looking for
+        for version in data['versions']:
+            if version['version'].startswith(f'{chrome_version}.'):
+                return version['version']
+            
+        # If we did not find a match, return default
+        return get_default_version(data)
+    else:
+        print(f'Request failed with status code {response.status_code}')
+
+def setup():
+    version = get_chrome_version_to_install()
+    os.system(f"wget --no-verbose -O /tmp/chrome.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_{version}-1_amd64.deb && apt install -y /tmp/chrome.deb && rm /tmp/chrome.deb")
+
+setup()
