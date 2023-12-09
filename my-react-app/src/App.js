4f68cdf44d573f468f0b9fc99d09d81100702084
skotww <scott.walraven@gmail.com>
2023-12-09 12:08:12 -0800

    intial front end

---
+++ my-react-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
