ee1e3ad0728c3f545a6ea8afcbb9796899d5c0f7
Ankit Jha <6148327+ankit-jha@users.noreply.github.com>
2024-03-19 02:01:52 +0000

    Removing scripts

--- scripts/install-dependencies.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-sudo apt-get update -y && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo apt install ./google-chrome-stable_current_amd64.deb -y
-pip3 install -r requirements.txt
-pip3 install -r requirements.txt -r tests/requirements.txt
