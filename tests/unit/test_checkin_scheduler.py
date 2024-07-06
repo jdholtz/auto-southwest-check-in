jdholtz/auto-southwest-check-in

commit 53ee2640aa2e9b66dd17e75923eb0cc02f7aa5c5

Use caching to speed up fare checks
Because a flight's reservation info is retrieved in the checkin scheduler
right before the fare checker retrieves the reservation info, this info
can be cached to remove the need for the fare checker to request this
info. This improves the speed of the fare checker and reduces strain
(and chance of detection) on Southwest's API due to the 33% decrease in
fare checker requests per flight.

lib/checkin_scheduler.py
@@ -55,20 +55,23 @@ class CheckInScheduler:
     def _get_flights(self, confirmation_number: str) -> List[Flight]:
         """Get all flights booked on a single reservation"""
         reservation_info = self._get_reservation_info(confirmation_number)
-        logger.debug("%d flights found under current reservation", len(reservation_info))
+        logger.debug("%d flights found under current reservation", len(reservation_info["bounds"]))
 
         current_utc_time = get_current_time()
         flights = []
         # If multiple flights are under the same confirmation number, it will schedule all checkins
-        for flight_info in reservation_info:
-            flight = Flight(flight_info, confirmation_number)
+        for flight_info in reservation_info["bounds"]:
+            # For simplicity, reservation_info is only cached in the Flight constructor even though
+            # it can get the flight_info
+            flight = Flight(flight_info, reservation_info, confirmation_number)
+
             if flight.departure_time > current_utc_time:
                 self._set_same_day_flight(flight, flights)
                 flights.append(flight)
 
         return flights
 
-    def _get_reservation_info(self, confirmation_number: str) -> List[Dict[str, Any]]:
+    def _get_reservation_info(self, confirmation_number: str) -> Dict[str, Any]:
         info = {
             "first-name": self.reservation_monitor.first_name,
             "last-name": self.reservation_monitor.last_name,
@@ -87,11 +90,10 @@ class CheckInScheduler:
             else:
                 logger.debug("Flights on the reservation have already departed")
 
-            return []
+            return {}
 
         logger.debug("Successfully retrieved reservation information")
-        reservation_info = response["viewReservationViewPage"]["bounds"]
-        return reservation_info
+        return response["viewReservationViewPage"]
 
     def _set_same_day_flight(self, flight: Flight, previous_flights: List[Flight]) -> None:
         for prev_flight in previous_flights:

tests/unit/test_checkin_scheduler.py
@@ -28,7 +28,7 @@ def test_flights(mocker: MockerFixture) -> List[Flight]:
         "departureTime": None,
         "flights": [{"number": "100"}],
     }
-    return [Flight(flight_info, ""), Flight(flight_info, "")]
+    return [Flight(flight_info, {}, ""), Flight(flight_info, {}, "")]
 
 
 class TestCheckInScheduler:
@@ -63,7 +63,9 @@ class TestCheckInScheduler:
     def test_get_flights_retrieves_all_flights_under_reservation(
         self, mocker: MockerFixture, test_flights: List[Flight]
     ) -> None:
-        mocker.patch.object(CheckInScheduler, "_get_reservation_info", return_value=[{}, {}])
+        mocker.patch.object(
+            CheckInScheduler, "_get_reservation_info", return_value={"bounds": [{}, {}]}
+        )
         mock_set_same_day_flight = mocker.patch.object(CheckInScheduler, "_set_same_day_flight")
 
         # Set the departing times to be after the current time
@@ -81,7 +83,9 @@ class TestCheckInScheduler:
     def test_get_flights_does_not_retrieve_departed_flights(
         self, mocker: MockerFixture, test_flights: List[Flight]
     ) -> None:
-        mocker.patch.object(CheckInScheduler, "_get_reservation_info", return_value=[{}, {}])
+        mocker.patch.object(
+            CheckInScheduler, "_get_reservation_info", return_value={"bounds": [{}, {}]}
+        )
 
         # Set the departing time to be before the current time. Only uses the first flight
         test_flights[0].departure_time = datetime(1999, 12, 30, 18, 29)
@@ -94,11 +98,11 @@ class TestCheckInScheduler:
         assert len(flights) == 0
 
     def test_get_reservation_info_returns_reservation_info(self, mocker: MockerFixture) -> None:
-        reservation_info = {"viewReservationViewPage": {"bounds": [{"test": "reservation"}]}}
-        mocker.patch("lib.checkin_scheduler.make_request", return_value=reservation_info)
+        reservation_content = {"viewReservationViewPage": {"bounds": [{"test": "reservation"}]}}
+        mocker.patch("lib.checkin_scheduler.make_request", return_value=reservation_content)
 
         reservation_info = self.scheduler._get_reservation_info("flight1")
-        assert reservation_info == [{"test": "reservation"}]
+        assert reservation_info == {"bounds": [{"test": "reservation"}]}
 
     # A reservation has flights in the past and this is the first time attempting to
     # schedule it
@@ -117,7 +121,7 @@ class TestCheckInScheduler:
         reservation_info = self.scheduler._get_reservation_info("flight1")
 
         mock_failed_reservation_retrieval.assert_called_once()
-        assert reservation_info == []
+        assert reservation_info == {}
 
     # A reservation is already scheduled but fails for a retrieval resulting in another error than
     # all flights being old
@@ -133,7 +137,7 @@ class TestCheckInScheduler:
         reservation_info = self.scheduler._get_reservation_info("flight1")
 
         mock_failed_reservation_retrieval.assert_called_once()
-        assert reservation_info == []
+        assert reservation_info == {}
 
     # A reservation is already scheduled and the flights are in the past
     def test_get_reservation_info_does_not_send_error_notification_when_reservation_is_old(
@@ -151,7 +155,7 @@ class TestCheckInScheduler:
         reservation_info = self.scheduler._get_reservation_info("flight1")
 
         mock_failed_reservation_retrieval.assert_not_called()
-        assert reservation_info == []
+        assert reservation_info == {}
 
     @pytest.mark.parametrize(["hour_diff", "same_day"], [(23, True), (24, True), (25, False)])
     def test_set_same_day_flight_sets_flight_as_same_day_correctly(

lib/fare_checker.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any, Dict, List, Tuple
 
-from .checkin_scheduler import VIEW_RESERVATION_URL
 from .flight import Flight
 from .log import get_logger
 from .utils import FlightChangeError, make_request
@@ -65,7 +64,7 @@ class FareChecker:
         Additionally, retrieve the flight's fare type so we can check the correct
         fare for a price drop.
         """
-        change_flight_page, fare_type_bounds = self._get_change_flight_page(flight)
+        change_flight_page, fare_type_bounds = self._get_change_flight_page(flight.reservation_info)
         query = self._get_search_query(change_flight_page, flight)
 
         info = change_flight_page["_links"]["changeShopping"]
@@ -83,16 +82,7 @@ class FareChecker:
         response = make_request("POST", site, self.headers, query, max_attempts=7)
         return response["changeShoppingPage"]["flights"][bound_page]["cards"], fare_type
 
-    def _get_change_flight_page(self, flight: Flight) -> Tuple[JSON, List[JSON]]:
-        # First, get the reservation information
-        logger.debug("Retrieving reservation information")
-        info = {
-            "first-name": self.reservation_monitor.first_name,
-            "last-name": self.reservation_monitor.last_name,
-        }
-        site = VIEW_RESERVATION_URL + flight.confirmation_number
-        response = make_request("GET", site, self.headers, info, max_attempts=7)
-        reservation_info = response["viewReservationViewPage"]
+    def _get_change_flight_page(self, reservation_info: JSON) -> Tuple[JSON, List[JSON]]:
         fare_type_bounds = reservation_info["bounds"]
 
         # Ensure the flight does not have a companion pass connected to it

lib/flight.py
@@ -20,12 +20,15 @@ class Flight:
     The flight time is automatically translated from the flight's local timezone to UTC.
     """
 
-    def __init__(self, flight_info: Dict[str, Any], confirmation_number: str) -> None:
+    def __init__(self, flight_info: JSON, reservation_info: JSON, confirmation_number: str) -> None:
         self.confirmation_number = confirmation_number
         self.departure_airport = flight_info["departureAirport"]["name"]
         self.destination_airport = flight_info["arrivalAirport"]["name"]
         self.flight_number = self._get_flight_number(flight_info["flights"])
         self.is_same_day = False
+
+        # Cached for use by the fare checker
+        self.reservation_info = reservation_info
 
         # Track to notify the user of filling out their passport information.
         # Southwest only fills the country's value for international flights

tests/integration/test_check_in.py
@@ -24,7 +24,7 @@ def handler(mocker: MockerFixture) -> None:
         "departureTime": "14:40",
         "flights": [{"number": "100"}],
     }
-    flight = Flight(flight_info, "TEST")
+    flight = Flight(flight_info, {}, "TEST")
     # Make sure it isn't affected by local time
     flight.departure_time = datetime(2021, 12, 6, 14, 40)
     return CheckInHandler(mock_scheduler, flight, Lock())

tests/integration/test_fare_checker.py
@@ -9,24 +9,14 @@ from unittest import mock
 import pytest
 from requests_mock.mocker import Mocker as RequestMocker
 
-from lib.checkin_scheduler import VIEW_RESERVATION_URL
 from lib.config import GlobalConfig
 from lib.fare_checker import BOOKING_URL, FareChecker
 from lib.flight import Flight
 from lib.reservation_monitor import ReservationMonitor
 from lib.utils import BASE_URL, FlightChangeError
 
-TEST_RESERVATION_URL = BASE_URL + VIEW_RESERVATION_URL + "TEST"
-CHAGE_FLIGHT_URL = BASE_URL + BOOKING_URL + "change_page"
+CHANGE_FLIGHT_URL = BASE_URL + BOOKING_URL + "change_page"
 MATCHING_FLIGHTS_URL = BASE_URL + BOOKING_URL + "matching_flights"
-
-RESERVATION_INFO = {
-    "viewReservationViewPage": {
-        "bounds": [{"fareProductDetails": {"fareProductId": "WGA"}}],
-        "_links": {"change": {"href": "change_page", "query": "test_query"}},
-        "greyBoxMessage": None,
-    }
-}
 
 CHANGE_FLIGHT_PAGE = {
     "changeFlightPage": {
@@ -93,15 +83,21 @@ def flight() -> Flight:
         "departureDate": "2021-12-06",
         "departureTime": "14:40",
         "flights": [{"number": "100"}, {"number": "101"}],
+        "fareProductDetails": {"fareProductId": "WGA"},
     }
-    return Flight(flight_info, "TEST")
+
+    reservation_info = {
+        "bounds": [flight_info],
+        "_links": {"change": {"href": "change_page", "query": "test_query"}},
+        "greyBoxMessage": None,
+    }
+    return Flight(flight_info, reservation_info, "TEST")
 
 
 def test_fare_drop_outbound(
     requests_mock: RequestMocker, monitor: ReservationMonitor, flight: Flight
 ) -> None:
-    requests_mock.get(TEST_RESERVATION_URL, [{"json": RESERVATION_INFO, "status_code": 200}])
-    requests_mock.get(CHAGE_FLIGHT_URL, [{"json": CHANGE_FLIGHT_PAGE, "status_code": 200}])
+    requests_mock.get(CHANGE_FLIGHT_URL, [{"json": CHANGE_FLIGHT_PAGE, "status_code": 200}])
     requests_mock.post(MATCHING_FLIGHTS_URL, [{"json": MATCHING_FLIGHTS, "status_code": 200}])
 
     fare_checker = FareChecker(monitor)
@@ -115,12 +111,11 @@ def test_fare_drop_inbound(
 ) -> None:
     flight.flight_number = "97"
 
-    bounds = [
-        {"fareProductDetails": {"fareProductId": "TEST"}},
-        {"fareProductDetails": {"fareProductId": "WGA"}},
-    ]
-    reservation_info = copy.deepcopy(RESERVATION_INFO)
-    reservation_info["viewReservationViewPage"]["bounds"] = bounds
+    flight_info = copy.deepcopy(flight.reservation_info["bounds"][0])
+    # Changing the outbound flight's fare to an invalid type will safeguard against the
+    # outbound flight being looked at instead of the inbound
+    flight_info["fareProductDetails"] = {"fareProductId": "TEST"}
+    flight.reservation_info["bounds"].insert(0, flight_info)
 
     flight_page = copy.deepcopy(CHANGE_FLIGHT_PAGE)
     page_info = flight_page["changeFlightPage"]
@@ -137,8 +132,7 @@ def test_fare_drop_inbound(
     matching_flights = copy.deepcopy(MATCHING_FLIGHTS)
     matching_flights["changeShoppingPage"]["flights"] = {"inboundPage": {"cards": FLIGHT_CARDS}}
 
-    requests_mock.get(TEST_RESERVATION_URL, [{"json": reservation_info, "status_code": 200}])
-    requests_mock.get(CHAGE_FLIGHT_URL, [{"json": flight_page, "status_code": 200}])
+    requests_mock.get(CHANGE_FLIGHT_URL, [{"json": flight_page, "status_code": 200}])
     requests_mock.post(MATCHING_FLIGHTS_URL, [{"json": matching_flights, "status_code": 200}])
 
     fare_checker = FareChecker(monitor)
@@ -163,8 +157,7 @@ def test_no_fare_drop(
     matching_flights = copy.deepcopy(MATCHING_FLIGHTS)
     matching_flights["changeShoppingPage"]["flights"]["outboundPage"]["cards"] = flights
 
-    requests_mock.get(TEST_RESERVATION_URL, [{"json": RESERVATION_INFO, "status_code": 200}])
-    requests_mock.get(CHAGE_FLIGHT_URL, [{"json": CHANGE_FLIGHT_PAGE, "status_code": 200}])
+    requests_mock.get(CHANGE_FLIGHT_URL, [{"json": CHANGE_FLIGHT_PAGE, "status_code": 200}])
     requests_mock.post(MATCHING_FLIGHTS_URL, [{"json": matching_flights, "status_code": 200}])
 
     fare_checker = FareChecker(monitor)
@@ -173,14 +166,9 @@ def test_no_fare_drop(
     monitor.notification_handler.lower_fare.assert_not_called()
 
 
-def test_flight_error_with_companion(
-    requests_mock: RequestMocker, monitor: ReservationMonitor, flight: Flight
-) -> None:
-    reservation_info = copy.deepcopy(RESERVATION_INFO)
+def test_flight_error_with_companion(monitor: ReservationMonitor, flight: Flight) -> None:
     message = {"body": "You must first cancel the associated companion reservation."}
-    reservation_info["viewReservationViewPage"]["greyBoxMessage"] = message
-
-    requests_mock.get(TEST_RESERVATION_URL, [{"json": reservation_info, "status_code": 200}])
+    flight.reservation_info["greyBoxMessage"] = message
 
     fare_checker = FareChecker(monitor)
     with pytest.raises(FlightChangeError):
@@ -188,12 +176,9 @@ def test_flight_error_with_companion(
 
 
 def test_flight_error_when_no_change_link_exists(
-    requests_mock: RequestMocker, monitor: ReservationMonitor, flight: Flight
+    monitor: ReservationMonitor, flight: Flight
 ) -> None:
-    reservation_info = copy.deepcopy(RESERVATION_INFO)
-    reservation_info["viewReservationViewPage"]["_links"]["change"] = None
-
-    requests_mock.get(TEST_RESERVATION_URL, [{"json": reservation_info, "status_code": 200}])
+    flight.reservation_info["_links"]["change"] = None
 
     fare_checker = FareChecker(monitor)
     with pytest.raises(FlightChangeError):
@@ -212,8 +197,7 @@ def test_unavailable_fares(
     matching_flights = copy.deepcopy(MATCHING_FLIGHTS)
     matching_flights["changeShoppingPage"]["flights"]["outboundPage"]["cards"] = flights
 
-    requests_mock.get(TEST_RESERVATION_URL, [{"json": RESERVATION_INFO, "status_code": 200}])
-    requests_mock.get(CHAGE_FLIGHT_URL, [{"json": CHANGE_FLIGHT_PAGE, "status_code": 200}])
+    requests_mock.get(CHANGE_FLIGHT_URL, [{"json": CHANGE_FLIGHT_PAGE, "status_code": 200}])
     requests_mock.post(MATCHING_FLIGHTS_URL, [{"json": matching_flights, "status_code": 200}])
 
     fare_checker = FareChecker(monitor)

tests/unit/test_fare_checker.py
@@ -25,7 +25,9 @@ def test_flight(mocker: MockerFixture) -> Flight:
         "departureTime": None,
         "flights": [{"number": "100"}],
     }
-    return Flight(flight_info, "")
+
+    reservation_info = {"bounds": [flight_info]}
+    return Flight(flight_info, reservation_info, "")
 
 
 class TestFareChecker:
@@ -93,7 +95,7 @@ class TestFareChecker:
 
     @pytest.mark.parametrize("bound", ["outbound", "inbound"])
     def test_get_matching_flights_retrieves_correct_bound_page(
-        self, mocker: MockerFixture, bound: str
+        self, mocker: MockerFixture, test_flight: Flight, bound: str
     ) -> None:
         change_flight_page = {"_links": {"changeShopping": {"href": "test_link"}}}
         fare_type_bounds = [
@@ -113,27 +115,23 @@ class TestFareChecker:
         response = {"changeShoppingPage": {"flights": {f"{bound}Page": {"cards": "test_cards"}}}}
         mocker.patch("lib.fare_checker.make_request", return_value=response)
 
-        matching_flights, fare_type = self.checker._get_matching_flights(None)
+        matching_flights, fare_type = self.checker._get_matching_flights(test_flight)
 
         assert matching_flights == "test_cards"
         assert fare_type == bound + "_fare"
 
     def test_get_change_flight_page_retrieves_change_flight_page(
-        self, mocker: MockerFixture, test_flight: Flight
+        self, mocker: MockerFixture
     ) -> None:
-        reservation_info = {
-            "viewReservationViewPage": {
-                "bounds": ["bound_one", "bound_two"],
-                "_links": {"change": {"href": "test_link", "query": "query"}},
-            }
+        res_info = {
+            "bounds": ["bound_one", "bound_two"],
+            "_links": {"change": {"href": "test_link", "query": "query"}},
         }
-        expected_page = {"changeFlightPage": "test_page"}
-        mock_make_request = mocker.patch(
-            "lib.fare_checker.make_request", side_effect=[reservation_info, expected_page]
-        )
+        flight_page = {"changeFlightPage": "test_page"}
+        mock_make_request = mocker.patch("lib.fare_checker.make_request", return_value=flight_page)
         mock_check_for_companion = mocker.patch.object(FareChecker, "_check_for_companion")
 
-        change_flight_page, fare_type_bounds = self.checker._get_change_flight_page(test_flight)
+        change_flight_page, fare_type_bounds = self.checker._get_change_flight_page(res_info)
 
         mock_check_for_companion.assert_called_once()
         assert change_flight_page == "test_page"
@@ -144,19 +142,16 @@ class TestFareChecker:
         assert call_args[3] == "query"
 
     def test_get_change_flight_page_raises_exception_when_flight_cannot_be_changed(
-        self, mocker: MockerFixture, test_flight: Flight
+        self, mocker: MockerFixture
     ) -> None:
         reservation_info = {
-            "viewReservationViewPage": {
-                "greyBoxMessage": None,
-                "bounds": ["bound_one", "bound_two"],
-                "_links": {"change": None},
-            }
+            "greyBoxMessage": None,
+            "bounds": ["bound_one", "bound_two"],
+            "_links": {"change": None},
         }
-        mocker.patch("lib.fare_checker.make_request", return_value=reservation_info)
 
         with pytest.raises(FlightChangeError):
-            self.checker._get_change_flight_page(test_flight)
+            self.checker._get_change_flight_page(reservation_info)
 
     def test_get_search_query_returns_the_correct_query_for_one_way(
         self, test_flight: Flight

tests/unit/test_flight.py
@@ -26,8 +26,10 @@ class TestFlight:
 
         # Needs to be mocked so it is only run when Flight is instantiated
         with mock.patch.object(Flight, "_set_flight_time"):
+            # Reservation info can be left empty as it is only used for caching, but isn't relevant
+            # to the functionality of the flight class
             # pylint: disable=attribute-defined-outside-init
-            self.flight = Flight(flight_info, "test_num")
+            self.flight = Flight(flight_info, {}, "test_num")
 
             # Flight times that would be set if _set_flight_time isn't mocked
             self.flight.departure_time = datetime(1971, 6, 18, 12)
@@ -44,7 +46,7 @@ class TestFlight:
             "departureTime": None,
             "flights": [{"number": "100"}],
         }
-        flight = Flight(flight_info, "")
+        flight = Flight(flight_info, {}, "")
 
         assert flight.is_international == is_international
 
@@ -58,8 +60,8 @@ class TestFlight:
             "departureTime": None,
             "flights": [{"number": "100"}],
         }
-        flight1 = Flight(flight_info, "")
-        flight2 = Flight(flight_info, "")
+        flight1 = Flight(flight_info, {}, "")
+        flight2 = Flight(flight_info, {}, "")
 
         flight1.departure_time = datetime(1999, 1, 1, 8, 59)
         flight2.departure_time = datetime(1999, 1, 1, 8, 59)
@@ -93,7 +95,7 @@ class TestFlight:
         self, mocker: MockerFixture, flight_info: Dict[str, Any], departure_time: datetime
     ) -> None:
         mocker.patch.object(Flight, "_set_flight_time")
-        new_flight = Flight(flight_info, "")
+        new_flight = Flight(flight_info, {}, "")
         new_flight.departure_time = departure_time
 
         assert self.flight != new_flight

commit f782b6bfbdd96501c3fcc3cfd3b152778e53df00

Fix KeyError when a request error occurs during reservation retrieval
This bug was introduced in 53ee2640aa2e9b66dd17e75923eb0cc02f7aa5c5.
When a RequestError occurred, empty JSON would be returned from
_get_reservation_info. However, _get_flights expected a bounds key. This
fix makes the bounds list empty if no key exists so the script doesn't
schedule any flights (and doesn't throw an exception).

lib/checkin_scheduler.py
@@ -52,12 +52,13 @@ class CheckInScheduler:
     def _get_flights(self, confirmation_number: str) -> List[Flight]:
         """Get all flights booked on a single reservation"""
         reservation_info = self._get_reservation_info(confirmation_number)
-        logger.debug("%d flights found under current reservation", len(reservation_info["bounds"]))
+        bounds = reservation_info.get("bounds", [])
+        logger.debug("%d flights found under current reservation", len(bounds))
 
         current_utc_time = get_current_time()
         flights = []
         # If multiple flights are under the same confirmation number, it will schedule all checkins
-        for flight_info in reservation_info["bounds"]:
+        for flight_info in bounds:
             # For simplicity, reservation_info is only cached in the Flight constructor even though
             # it can get the flight_info
             flight = Flight(flight_info, reservation_info, confirmation_number)

tests/unit/test_checkin_handler.py
@@ -161,7 +161,7 @@ class TestCheckInHandler:
     def test_check_in_sends_error_notification_when_check_in_fails(
         self, mocker: MockerFixture
     ) -> None:
-        mocker.patch.object(CheckInHandler, "_attempt_check_in", side_effect=RequestError("", ""))
+        mocker.patch.object(CheckInHandler, "_attempt_check_in", side_effect=RequestError(""))
         mock_notification_handler = mocker.patch("lib.notification_handler.NotificationHandler")
 
         self.handler.notification_handler = mock_notification_handler

tests/unit/test_checkin_scheduler.py
@@ -78,6 +78,13 @@ class TestCheckInScheduler:
         assert len(flights) == 2
         assert mock_set_same_day_flight.call_count == len(flights)
 
+    def test_get_flights_schedules_no_flights_on_request_error(self, mocker: MockerFixture) -> None:
+        mocker.patch("lib.checkin_scheduler.make_request", side_effect=RequestError(""))
+
+        flights = self.scheduler._get_flights("flight1")
+
+        assert len(flights) == 0
+
     def test_get_flights_does_not_retrieve_departed_flights(
         self, mocker: MockerFixture, test_flights: List[Flight]
     ) -> None:
@@ -126,7 +133,7 @@ class TestCheckInScheduler:
     def test_get_reservation_info_sends_error_when_reservation_retrieval_fails_and_flight_scheduled(
         self, mocker: MockerFixture, test_flights: List[Flight]
     ) -> None:
-        mocker.patch("lib.checkin_scheduler.make_request", side_effect=RequestError("", ""))
+        mocker.patch("lib.checkin_scheduler.make_request", side_effect=RequestError(""))
         mock_failed_reservation_retrieval = mocker.patch.object(
             NotificationHandler, "failed_reservation_retrieval"
         )

tests/unit/test_reservation_monitor.py
@@ -174,7 +174,7 @@ class TestReservationMonitor:
 
         assert mock_check_flight_price.call_count == len(self.monitor.checkin_scheduler.flights)
 
-    @pytest.mark.parametrize("exception", [RequestError("", ""), FlightChangeError, Exception])
+    @pytest.mark.parametrize("exception", [RequestError(""), FlightChangeError, Exception])
     def test_check_flight_fares_catches_error_when_checking_fares(
         self, mocker: MockerFixture, exception: Exception
     ) -> None: