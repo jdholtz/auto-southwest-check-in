2f42bfd410d998cb9df8f88bd28d65e1736e85b4
jdholtz <joeyholtzman0507@gmail.com>
2025-05-27 17:58:12 -0700

    Fix typo

--- tests/unit/test_reservation_monitor.py
+++ tests/unit/test_reservation_monitor.py
@@ -202,7 +202,7 @@ class TestReservationMonitor:
             "lib.reservation_monitor.get_current_time", return_value=datetime(1999, 12, 30, 12)
         )
 
-        # Retrival interval is 1 hour, but the time taken is more than an hour
+        # Retrieval interval is 1 hour, but the time taken is more than an hour
         self.monitor.config.retrieval_interval = 60 * 60
         self.monitor._smart_sleep(datetime(1999, 12, 30, 10))
 
